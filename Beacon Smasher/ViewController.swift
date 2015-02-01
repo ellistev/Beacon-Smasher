8f00da27ffdea62bd96837d22ad387e93ce2f837
Steven Elliott <steven.peter.elliott@gmail.com>
2015-01-31 19:53:35 -0700

    Initial Commit

---
+++ Beacon Smasher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beacon Smasher
+//
+//  Created by steven elliott on 2015-01-31.
+//  Copyright (c) 2015 Steven Peter Elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
