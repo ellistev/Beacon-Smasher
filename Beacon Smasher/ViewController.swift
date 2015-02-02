70973d892622adac6df3e8f2942095db8159b934
Steven Elliott <steven.peter.elliott@gmail.com>
2015-02-01 21:46:06 -0700

    moved to more mvc view

--- Beacon Smasher/ViewController.swift
+++ Beacon Smasher/ViewController.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
-import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
-    let locationManager = CLLocationManager()
-    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "f7826da6-4fa2-4e98-8024-bc5b71e0893e"), identifier: "Kontakt.io")
+
+class ViewController: UIViewController {
+
     let colors = [
         62288: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
         36039: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
@@ -21,35 +20,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //start scanning right away
-        locationManager.delegate = self;
-        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
-            locationManager.requestWhenInUseAuthorization()
-        }
-        locationManager.startRangingBeaconsInRegion(region)
+        var beaconController = BeaconController()
         
         
-        
+
     }
     
     @IBOutlet weak var beaconsFoundText: UILabel!
     
-    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
-        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
-        if (knownBeacons.count > 0) {
-            let closestBeacon = knownBeacons[0] as CLBeacon
-            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
-            beaconsFoundText.text = closestBeacon.description + " " + closestBeacon.proximityUUID.description + " " + closestBeacon.major.description + " " + closestBeacon.minor.description + " " + closestBeacon.proximity.hashValue.description
-            
-            
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
